440a3402c27dc6e6156fd9cad5fb072fdf7aa4cd
Miles Smith <mileschristiansmith@gmail.com>
2020-07-10 10:29:00 -0500

    .
    Former-commit-id: a60a383f4dc5cb077094bd91b1f951e216723151 [formerly cdc1d0e4ee1d98d8d3a2f90b153d0466c78c6320]
    Former-commit-id: c9567ba1e7fe61c595df0bc96b1dced1a653f627
    Former-commit-id: 8b98f628b0e12f37090264f758f0a40af8f6f71c

---
+++ cupcake/__about__.py
@@ -0,0 +1,6 @@
+r"""*Single-source version number for* ``cdna_cupcake``.
+"""
+
+__author__ = "Elizabeth Tseng"
+__email__ = "etseng@pacb.com"
+__version__ = "12.2.8"
--- cupcake/cupcake/__init__.py
+++ cupcake/cupcake/__init__.py
@@ -5,3 +5,11 @@ __version__ = "8.6"
 #     io,
 #     tofu,
 # )
+
+try:
+    with open(osp.join("cupcake", "__about__.py")) as f:
+        exec(f.read())
+except:
+    __author__ = "Elizabeth Tseng"
+    __email__ = "etseng@pacb.com"
+    __version__ = "unknown"
