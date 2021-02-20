64af3a8e5e971c98769d29d3b02c9f922130d22a
milescsmith <mileschristiansmith@gmail.com>
2021-02-20 09:37:02 -0600

    add missing build script
    Former-commit-id: ee61f7a74ff24d68ee1f9e337f28bb6124bf5ae7 [formerly 6b8dce15bbf5ea31e418295e817de92e1e1a0414]
    Former-commit-id: 20e10543674b8b7441f7a80653f16c8294be16f1
    Former-commit-id: 0c7b57ef4b9d3885b387f7348085611780f28537

---
+++ build.py
@@ -0,0 +1,80 @@
+# lifted from https://github.com/sdispater/pendulum
+import shutil
+import numpy as np
+from Cython.Build import cythonize
+from pathlib import Path
+
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution
+from distutils.core import Extension
+from distutils.errors import CCompilerError
+from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsPlatformError
+
+
+# C Extensions
+
+extensions = [
+    Extension("cupcake.tofu.branch.intersection_unique", include_dirs=[np.get_include()], sources=["src/cupcake/tofu/branch/intersection_unique.pyx"]),
+    Extension("cupcake.tofu.branch.c_branch", include_dirs=[np.get_include()], sources=["src/cupcake/tofu/branch/c_branch.pyx"]),
+]
+
+
+class BuildFailed(Exception):
+    pass
+
+
+class ExtBuilder(build_ext):
+    # This class allows C extension building to fail.
+
+    built_extensions = []
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError):
+            print(
+                "  Unable to build the C extensions, "
+                "cDNA_Cupcake will use the pure python code instead."
+            )
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
+            print(
+                '  Unable to build the "{}" C extension, '
+                "cDNA_Cupcake will use the pure python version of the extension.".format(
+                    ext.name
+                )
+            )
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    distribution = Distribution({"name": "src/cupcake", "ext_modules": cythonize(extensions)})
+    distribution.package_dir = "cupcake"
+
+    cmd = ExtBuilder(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        output = Path(output)
+        relative_extension = Path("src").joinpath(output.relative_to(cmd.build_lib))
+        if not output.exists():
+            continue
+
+        shutil.copyfile(output, relative_extension)
+        mode = relative_extension.stat().st_mode
+        mode |= (mode & 0o444) >> 2
+        relative_extension.chmod(mode)
+
+    return setup_kwargs
+
+
+if __name__ == "__main__":
+    build({})
