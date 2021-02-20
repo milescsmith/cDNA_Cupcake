72a0fce1101ff4894d03b5df4a4c98893c1b8819
milescsmith <mileschristiansmith@gmail.com>
2021-02-19 22:06:44 -0600

    add poetry-based pyproject.toml
    Former-commit-id: d483dcb78ee6572a44d98f6b95728550ac63cd56 [formerly 28afe6811db8d4981705e56a0e0303e953f38b34]
    Former-commit-id: 64866d874c6798b169475698b827181aa5e82ca9
    Former-commit-id: 363b08dd0300a268c5fb446f4bec1b3a22c9e342

--- setup.py
+++ setup.py
@@ -1,90 +1,97 @@
-from glob import glob
-from pathlib import Path
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-import numpy as np
-from Cython.Build import cythonize
-from setuptools import Extension, find_packages, setup
+package_dir = \
+{'': 'src'}
 
-try:
-    exec(Path("src", "cupcake", "__about__.py").read_text())
-except FileNotFoundError:
-    __author__ = "Elizabeth Tseng"
-    __email__ = "etseng@pacb.com"
-    __version__ = "19.0.0"
+packages = \
+['cupcake',
+ 'cupcake.SequelQC',
+ 'cupcake.annotation',
+ 'cupcake.bacteria',
+ 'cupcake.beta',
+ 'cupcake.phasing',
+ 'cupcake.phasing.io',
+ 'cupcake.phasing.utils',
+ 'cupcake.post_isoseq_cluster',
+ 'cupcake.sequence',
+ 'cupcake.simulate',
+ 'cupcake.singlecell',
+ 'cupcake.targeted',
+ 'cupcake.tofu',
+ 'cupcake.tofu.branch',
+ 'cupcake.tofu.counting']
 
+package_data = \
+{'': ['*'], 'cupcake.annotation': ['test_data/*']}
 
-EXT_MODULES = [
-    Extension(
-        "cupcake.tofu.branch.intersection_unique",
-        ["src/cupcake/tofu/branch/intersection_unique.pyx"],
-    ),
-    Extension(
-        "cupcake.tofu.branch.c_branch", ["src/cupcake/tofu/branch/c_branch.pyx"],
-    ),
-]
+install_requires = \
+['Cython>=0.29.21,<0.30.0',
+ 'bcbio-gff>=0.6.6,<0.7.0',
+ 'biopython>=1.78,<2.0',
+ 'bx-python>=0.8.9,<0.9.0',
+ 'numpy>=1.20.1,<2.0.0',
+ 'pandas>=1.2.2,<2.0.0',
+ 'pbcommand @ git+https://github.com/PacificBiosciences/pbcommand@master',
+ 'pbcore @ git+https://github.com/PacificBiosciences/pbcore@master',
+ 'pbcoretools @ git+https://github.com/PacificBiosciences/pbcoretools@master',
+ 'pysam>=0.16.0,<0.17.0',
+ 'pyvcf @ git+https://github.com/milescsmith/PyVCF@master',
+ 'scikit-learn>=0.24.1,<0.25.0']
 
-setup(
-    name="cupcake",
-    version=__version__,
-    author=__author__,
-    author_email=__email__,
-    description="Miscellaneous collection of Python and R scripts for processing Iso-Seq data",
-    long_description=Path("README.md").read_text("utf-8"),
-    url="https://github.com/Magdoll/cDNA_Cupcake",
-    ext_modules=cythonize(EXT_MODULES),
-    include_dirs=[np.get_include()],
-    zip_safe=False,
-    python_requires="~=3.7",
-    license="BSD3",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-    keywords=["isoseq", "rnaseq", "pacbio", "long reads"],
-    packages=find_packages("src"),
-    package_data={"": ["tests/test_data/*.*"]},
-    package_dir={"cupcake": "src/cupcake"},
-    # py_modules=[Path(path).stem for path in glob("src/cupcake/*.py")],
-    include_package_data=True,
-    setup_requires=["numpy", "cython", "setuptools_scm"],
-    # setup_requires=["cython", "setuptools_scm"],
-    install_requires=[
-        line.strip()
-        for line in Path("requirements.txt").read_text("utf-8").splitlines()
-    ],
-    # this might be better to reformat into a series of subcommands
-    entry_points={
-        "console_scripts": [
-            # annotation submodule
-            "evaluate_alignment_sam      = cupcake.annotation.alignment_stats_from_sam:main",
-            "make_file_for_subsample     = cupcake.annotation.make_file_for_subsampling_from_collapsed:main",
-            "parse_matchAnnot            = cupcake.annotation.parse_matchAnnot:main",
-            "subsample_with_category     = cupcake.annotation.subsample_with_category:main",
-            "subsample                   = cupcake.annotation.subsample:main",
-            # tofu submodule
-            "collapse_isoforms_by_sam    = cupcake.tofu.collapse_isoforms_by_sam:main",
-            "get_abundance_post_collapse = cupcake.tofu.get_abundance_post_collapse:main",
-            "filter_by_count             = cupcake.tofu.filter_by_count:main",
-            "filter_away_subset          = cupcake.tofu.filter_away_subset:main",
-            "fusion_finder               = cupcake.tofu.fusion_finder:main",
-            "chain_samples               = cupcake.tofu.counting.chain_samples:main",
-            "chain_fusion_samples        = cupcake.tofu.counting.chain_fusion_samples:main",
-            "summarize_junctions         = cupcake.tofu.counting.summarize_sample_GFF_junctions:main",
-            "scrub_sample_GFFs           = cupcake.tofu.counting.scrub_sample_GFF_junctions:main",
-            # uh, phasing! are we still doing phasing?
-            "make_fake_genome            = cupcake.phasing.create_fake_genome:main",
-            "simple_stats_post_collapse  = cupcake.tofu.simple_stats_post_collapse:main",
-            "fusion_collate_info         = cupcake.tofu.fusion_collate_info:main",
-            "color_bed12_post_sqanti     = cupcake.tofu.color_bed12_post_sqanti:main",
-            "run_phaser                  = cupcake.phasing.run_phaser:main",
-        ]
-    },
-)
+entry_points = \
+{'console_scripts': ['chain_fusion_samples = '
+                     'cupcake.tofu.counting.chain_fusion_samples:main',
+                     'chain_samples = cupcake.tofu.counting.chain_samples:main',
+                     'collapse_isoforms_by_sam = '
+                     'cupcake.tofu.collapse_isoforms_by_sam:main',
+                     'color_bed12_post_sqanti = '
+                     'cupcake.tofu.color_bed12_post_sqanti:main',
+                     'evaluate_alignment_sam = '
+                     'cupcake.annotation.alignment_stats_from_sam:main',
+                     'filter_away_subset = '
+                     'cupcake.tofu.filter_away_subset:main',
+                     'filter_by_count = cupcake.tofu.filter_by_count:main',
+                     'fusion_collate_info = '
+                     'cupcake.tofu.fusion_collate_info:main',
+                     'fusion_finder = cupcake.tofu.fusion_finder:main',
+                     'get_abundance_post_collapse = '
+                     'cupcake.tofu.get_abundance_post_collapse:main',
+                     'make_fake_genome = '
+                     'cupcake.phasing.create_fake_genome:main',
+                     'make_file_for_subsample = '
+                     'cupcake.annotation.make_file_for_subsampling_from_collapsed:main',
+                     'parse_matchAnnot = '
+                     'cupcake.annotation.parse_matchAnnot:main',
+                     'run_phaser = cupcake.phasing.run_phaser:main',
+                     'scrub_sample_GFFs = '
+                     'cupcake.tofu.counting.scrub_sample_GFF_junctions:main',
+                     'simple_stats_post_collapse = '
+                     'cupcake.tofu.simple_stats_post_collapse:main',
+                     'subsample = cupcake.annotation.subsample:main',
+                     'subsample_with_category = '
+                     'cupcake.annotation.subsample_with_category:main',
+                     'summarize_junctions = '
+                     'cupcake.tofu.counting.summarize_sample_GFF_junctions:main']}
+
+setup_kwargs = {
+    'name': 'cupcake',
+    'version': '19.0.2',
+    'description': 'Miscellaneous collection of Python and R scripts for processing Iso-Seq data',
+    'long_description': None,
+    'author': 'Elizabeth Tseng',
+    'author_email': 'etseng@pacb.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7.9,<4.0.0',
+}
+from build import *
+build(setup_kwargs)
+
+setup(**setup_kwargs)
