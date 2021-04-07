d37d8d8a4dd39f0b3ea4abcb342fadb6c3c2ac16
Miles Smith <miles-smith@omrf.org>
2021-04-07 17:11:10 -0500

    fix: use importlib.metadata.metadata properly
    Former-commit-id: a197b1522f0202e2b4c0d5d3f9a61d1070adf51e
    Former-commit-id: b509cdcdd252c5edf56bb3b683d5697391b55a71
    Former-commit-id: 707822459ce25f956fb52c6c31c347c3dd5a6b3a

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [21.05.5] - 2021-04-07
+
+### Fixed
+- Hot fix for importlib.metadata.metadata usage
+
 ## [21.0.4] - 2021-04-07
 
 ### Changed
--- src/cupcake/__about__.py
+++ src/cupcake/__about__.py
@@ -7,6 +7,6 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     from importlib_metadata import metadata
 
-__author__ = metadata["Author"]
-__email__ = metadata["Author-email"]
-__version__ = metadata["Version"]
+__author__ = metadata("cupcake")["Author"]
+__email__ = metadata("cupcake")["Author-email"]
+__version__ = metadata("cupcake")["Version"]
